a3001af6e4c6f622d749801c6c8c3fe4135e7be9
NishanthAdithya <144264872+NishanthAdithya@users.noreply.github.com>
2025-11-29 00:59:21 +0000

    Deploying to gh-pages from @ NishanthAdithya/nishanthadithya.github.io@ac327fd954943c4af1d591e908734ac7fa36a3ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-teaching",
-          title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
